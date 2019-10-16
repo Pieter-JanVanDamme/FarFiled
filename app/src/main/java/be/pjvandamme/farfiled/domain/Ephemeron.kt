2750ff56ca7431d07048948a02c0b6bf23d248e3
Pieter-Jan Van Damme <pieterjan.vandamme.p4157@student.hogent.be>
2019-10-16 09:58:01 +0200

    [feat] Addition of static information to data class Ephemeron, as companion object.

--- app/src/main/java/be/pjvandamme/farfiled/domain/Ephemeron.kt
+++ app/src/main/java/be/pjvandamme/farfiled/domain/Ephemeron.kt
@@ -4,4 +4,12 @@ data class Ephemeron(
     val relation: Relation,
     val title: String,
     val content: String
-)
+){
+    companion object{
+        val description: String = "currently relevant topics"
+        val detailedDescription: String = "currently relevant projects/circumstances or latest " +
+                "developments in some area, topics of conversation, anything important for " +
+                "future interactions (promises/debts/favors owed, important dates in the " +
+                "near-future)"
+    }
+}
